9894e43741ed6cd69ccf68083ddb973a4bff29b0
bulo4ka <79746417+juniperbotik@users.noreply.github.com>
2024-01-06 02:15:32 +0600

    Update script.py

--- script.py
+++ script.py
@@ -4,22 +4,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 # Список URL-ов для получения прокси
 proxy_urls = [
-    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
-    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
-    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
-    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
-    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
-    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
-    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
-    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
-    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4",
-    "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5",
-    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
-    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
-    "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
-    "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt",
-    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
-    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt"
+    "https://juniperbotik.github.io/cskphpbalance"
 ]
 
 # Указываем абсолютный путь к файлу proxies.txt
